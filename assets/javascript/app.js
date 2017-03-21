864dba20986d6e7ba618cd8273574acf6d3c6d47
Robert Geyer <Robert Geyer>
2017-03-21 15:09:01 -0400

    added shadows, correct images

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -17,33 +17,37 @@ $( document ).ready(function() {
 	    question: "Who is the main character of Uncharted?",
 	    choices: ["Nathan Drake", "Aloy", "Master Chief", "Mario"],
 	    correctAnswer: "Nathan Drake",
-	    image: "<img src='assets/images/nathanDrake.jpg' class='center-block'>"
+	    image: "<img src='assets/images/nathanDrake.jpg' class='img-circle shadow'>"
 	  }, 
 	  {
-	    question: "What is the name of the fictional English archaeologist in the game ‘Tomb Raider’?",
+	    question: "What is the name of the fictional English archaeologist in the game Tomb Raider?",
 	    choices: ["Cortana", "Lara Croft", "Faith", "Kitana"],
-	    correctAnswer: "Lara Croft"
+	    correctAnswer: "Lara Croft",
+	    image: "<img src='assets/images/laraCroft.jpg' class='img-circle shadow'>"
 	  }, 
 	  {
-	    question: "‘Black Ops’ is the subtitle of which game?",
+	    question: "Black Ops is the subtitle of which game?",
 	    choices: ["Call of Duty", "Battlefield", "Operation:", "Sims 4"],
-	    correctAnswer: "Call of Duty"
+	    correctAnswer: "Call of Duty",
+	    image: "<img src='assets/images/blackOps.jpg' class='img-circle shadow'>"
 	  }, 
 	  {
 	    question: "Pikachu is one of the species of creatures in which series of games?",
 	    choices: ["Digimon", "Space Invaders", "Rick & Morty", "Pokemon"],
-	    correctAnswer: "Pokemon"
+	    correctAnswer: "Pokemon",
+	    image: "<img src='assets/images/pikachu.jpg' class='img-circle shadow'>"
 	  }, 
 	  {
 	    question: "What color is Pac-Man?",
 	    choices: ["Pink", "Blue", "Burnt Orange", "Yellow"],
-	    correctAnswer: "Yellow"
+	    correctAnswer: "Yellow",
+	    image: "<img src='assets/images/pacMan.png' class='img-circle shadow'>"
 	  }];
 	  
 
 	// create question contents
 	function questionContent() {
-    	$("#gameScreen").append("<p class'questionHeader'>" + questions[questionCounter].question + "</p><p class='choices'>" + questions[questionCounter].choices[0] + "</p><p class='choices'>" + questions[questionCounter].choices[1] + "</p><p class='choices'>" + questions[questionCounter].choices[2] + "</p><p class='choices'>" + questions[questionCounter].choices[3] + "</p>");
+    	$("#gameScreen").append("<p class'questionHeader'><strong>" + questions[questionCounter].question + "</p><p class='choices'>" + questions[questionCounter].choices[0] + "</p><p class='choices'>" + questions[questionCounter].choices[1] + "</p><p class='choices'>" + questions[questionCounter].choices[2] + "</p><p class='choices'>" + questions[questionCounter].choices[3] + "</strong></p>");
 	}
 
 	// user guessed correctly
@@ -51,8 +55,9 @@ $( document ).ready(function() {
 		$("#gameScreen").html("<p>You got it right!</p>");
 		correctGuesses++;
 		var correctAnswer = questions[questionCounter].correctAnswer;
-		$("#gameScreen").append("<p>The right answer was <span class='answer'>" + correctAnswer + "</span></p>" + questions[questionCounter].image);
+		$("#gameScreen").append("<p>The answer was <span class='answer'><strong>" + correctAnswer + "</strong></span></p>" + questions[questionCounter].image);
 		setTimeout(nextQuestion, 5000);
+		questionCounter++;
 	}
 
 	// user guessed incorrectly
@@ -60,8 +65,9 @@ $( document ).ready(function() {
 		$("#gameScreen").html("<p>Nope, that's not it!</p>");
 		incorrectGuesses++;
 		var correctAnswer = questions[questionCounter].correctAnswer;
-		$("#gameScreen").append("<p>The right answer was <span class='answer'>" + correctAnswer + "</span></p>" + questions[questionCounter].image);
+		$("#gameScreen").append("<p>The answer was <span class='answer'><strong>" + correctAnswer + "</strong></span></p>" + questions[questionCounter].image);
 		setTimeout(nextQuestion, 5000);
+		questionCounter++;
 	}
 
 	// user ran out of time
@@ -70,13 +76,14 @@ $( document ).ready(function() {
 			$("#gameScreen").html("<p>You ran out of time!</p>");
 			incorrectGuesses++;
 			var correctAnswer = questions[questionCounter].correctAnswer;
-			$("#gameScreen").append("<p>The right answer was <span class='answer'>" + correctAnswer + "</span></p>" + questions[questionCounter].image);
+			$("#gameScreen").append("<p>The answer was <span class='answer'><strong>" + correctAnswer + "</strong></span></p>" + questions[questionCounter].image);
 			setTimeout(nextQuestion, 5000);
+			questionCounter++;
 		}
 	}
 
 	function resultsScreen() {
-		$("#gameScreen").html("<p>Good work!</p>" + "<p>You got " + correctGuesses + " right.</p>" + "<p>You got " + incorrectGuesses + " wrong.</p>");
+		$("#gameScreen").html("<p>Good work!</p>" + "<p>You got <strong>" + correctGuesses + "</strong> right.</p>" + "<p>You got <strong>" + incorrectGuesses + "</strong> wrong.</p>");
 		$("#gameScreen").append("<h1 id='start'>Start Over?</h1>");
 		gameReset();
 		$("#start").click(nextQuestion);
@@ -93,15 +100,14 @@ $( document ).ready(function() {
 			if (time > 0) {
 				time--;
 			}
-			$("#timer").html(time);
+			$("#timer").html("<strong>" + time + "</strong>");
 		}
 	}
 
 	function nextQuestion() {
-		if (questionCounter < 4) {
+		if (questionCounter < 5) {
 			time = 30;
-			$("#gameScreen").html("<p>You have <span id='timer'>" + time + "</span> seconds left!</p>");
-			questionCounter++;
+			$("#gameScreen").html("<p>You have <span id='timer'><strong>" + time + "</strong></span> seconds left!</p>");
 			questionContent();
 			timer();
 			userTimeout();
